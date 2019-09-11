c917e29135ca8d0f5557f50120c6eb9d6de0b855
Dean Gurvitz <deansg@gmail.com>
2019-09-11 17:38:57 +0300

    Improve startup script

--- Run App.bat
+++ Run App.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\MATLAB\R2019a\bin\MATLAB.exe" -nosplash -nodesktop -r "cd('%cd%'); run('app\startWindow.m');"
+SET "PATH=%PATH%;C:\Program Files\MATLAB\R2018b\bin;C:\Program Files\MATLAB\R2019a\bin"
+MATLAB.exe -nosplash -nodesktop -r "cd('%cd%'); run('app\startWindow.m');"
